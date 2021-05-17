9fa03dea29b08ade8fa94eef13754300791fda24
Liz Burton <liz.burton147@gmail.com>
2021-05-17 11:32:55 -0400

    comment out bonus test

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -34,15 +34,16 @@ RSpec.describe "Students", type: :request do
     end
   end
   
-  # remove skip: "bonus" to run the test for the bonus!
-  describe "GET /students/highest-grade", skip: "bonus" do
-    it 'returns the student with the highest grade' do
-      get '/students/highest-grade'
+  ## BONUS: un-comment out the code below to run the bonus test
 
-      expect(response.body).to include_json({ 
-        first_name: 'Idris', last_name: 'Elba', grade: 105 
-      })
-    end
-  end
+  # describe "GET /students/highest-grade" do
+  #   it 'returns the student with the highest grade' do
+  #     get '/students/highest-grade'
+
+  #     expect(response.body).to include_json({ 
+  #       first_name: 'Idris', last_name: 'Elba', grade: 105 
+  #     })
+  #   end
+  # end
   
 end
